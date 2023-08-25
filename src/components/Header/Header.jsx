9a02b2ebc0f562ec4bf077430e7f818a74cca4df
shahariaz <shahariaz.info@gmail.com>
2023-08-26 02:26:27 +0600

    nav bar or haeader done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import "./Header.css";
 const Header = () => {
   return (
     <section className="h-wrapper">
-      <div className="h-container">
+      <div className="h-container flexCenter innerWidth paddings">
         <img src="./logo.png" alt="logo" width={100} />
-        <div className="h-menu">
+        <div className="h-menu flexCenter">
           <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
