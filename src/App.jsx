61655d967e40a0e37afdbd5ced1378c7c5925d5a
shahariaz <shahariaz.info@gmail.com>
2023-08-25 11:25:52 +0600

    pushing file stracture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="./logo.png" alt="logo" width={100} />
+        <div className="h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Value</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
